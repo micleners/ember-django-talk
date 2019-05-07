95943da3054234d61e2de255670ca5135303a373
mbeche <mb.johnsonbece@gmail.com>
2019-05-07 18:42:18 -0500

    Commit 2: Backend API endpoint created for events

--- events-app/events-backend/events/models.py
+++ events-app/events-backend/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# basic information for our events
+class Event(models.Model):
+    # Only required field is title
+    title = models.CharField(max_length=256)
+    presenter = models.CharField(max_length=256, blank=True)
+    # Default time is now if not provided
+    time = models.DateTimeField(default=timezone.now)
+    location = models.CharField(max_length=256, blank=True)
+    description = models.TextField(blank=True)
