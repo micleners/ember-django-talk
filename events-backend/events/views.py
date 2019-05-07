fd78cafb6a25878e1fd2f9d4e2ad53b61241b4c4
Michael Johnson <mb.johnsonbece@gmail.com>
2019-05-06 20:58:48 -0500

    Backend API endpoint created for events

--- events-backend/events/views.py
+++ events-backend/events/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .models import Event
+from .serializers import UserSerializer, EventSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    resource_name = 'events'
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
