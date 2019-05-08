312287b22d69aa1965ec37ae2fe5d7207fb8e6cc
mbeche <mb.johnsonbece@gmail.com>
2019-05-07 19:23:31 -0500

    Commit 5: Made events route. Need CORS headers

---
+++ events-app/events-frontend/app/adapters/application.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+
+export default DS.RESTAdapter.extend({
+  host: computed(function(){
+    return 'http://localhost:8000';
+  }),
+  namespace: 'api',
+  buildURL: function(type, id, record) {
+  //call the default buildURL and then append a slash
+  return this._super(type, id, record) + '/';
+  }
+});
